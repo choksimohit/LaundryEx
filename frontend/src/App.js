53b6ec580e699cec7de449362b5020aac2d311c8
choksimohit <er.choksimohit@gmail.com>
2026-06-08 02:19:48 +0530

    remove Vercel Analytics component from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from './components/ui/sonner';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
@@ -80,7 +79,6 @@ function App() {
         </div>
         <Footer />
         <Toaster position="top-center" />
-        <Analytics />
       </div>
     </BrowserRouter>
   );
